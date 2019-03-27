8566eeb114ca2afb2862fbf36fd8f6bfdaedadd7
Nina <ninarachaebuie@gmail.com>
2019-03-26 20:02:12 -0500

    Updates

---
+++ precache-manifest.57dfc40734aae94dff2846d9d9a043f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcb1692c32e9335e653f644cfb13d144",
+    "url": "/berry-display/static/media/symphony.fcb1692c.png"
+  },
+  {
+    "revision": "566a6795c944f85bb914",
+    "url": "/berry-display/static/js/runtime~main.0b1136df.js"
+  },
+  {
+    "revision": "f1e05373db800767c059",
+    "url": "/berry-display/static/js/main.fddced09.chunk.js"
+  },
+  {
+    "revision": "3542de56fae3f6fb2dab",
+    "url": "/berry-display/static/js/2.7fb1241b.chunk.js"
+  },
+  {
+    "revision": "f1e05373db800767c059",
+    "url": "/berry-display/static/css/main.5431052d.chunk.css"
+  },
+  {
+    "revision": "3d97cd80cc8e161bbf6b168260ce8f28",
+    "url": "/berry-display/index.html"
+  }
+];
